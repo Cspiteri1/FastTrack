599b66ac26728eadf5e3680970684d90515fc13e
Cspiteri1 <cspiteri_16@hotmail.com>
2023-12-18 15:28:06 +0100

    Created Questions and Answers
    Created the Structure of Questions and Answers following some functions relatively. Created some sample data aswell

--- main.go
+++ main.go
@@ -13,6 +13,16 @@ type player struct {
 	Score int    `json:"score"`
 }
 
+type question struct {
+	QuestionText string
+	Answers      []answer
+}
+
+type answer struct {
+	AnswerText string
+	Valid      bool
+}
+
 func createPlayer(name string, age int, score int) player {
 	p := player{
 		Name:  name,
@@ -23,14 +33,49 @@ func createPlayer(name string, age int, score int) player {
 	return p
 }
 
-var players []player
+func createQuestion(text string, answer []answer) question {
+	q := question{
+		QuestionText: text,
+		Answers:      answer,
+	}
+	return q
+}
 
-func main() {
+func createAnswer(text string, valid bool) answer {
+	a := answer{
+		AnswerText: text,
+		Valid:      valid,
+	}
+	return a
+}
+
+func initData() {
+
 	players = append(players, createPlayer("Clayton", 28, 60))
 	players = append(players, createPlayer("Giancarl", 27, 40))
 	players = append(players, createPlayer("Emma", 33, 100))
 	players = append(players, createPlayer("Paulinha", 30, 80))
 
+	var answersGroupA []answer = []answer{createAnswer("Italian", false), createAnswer("Arabic", false), createAnswer("Maltese", true)}
+	var answersGroupB []answer = []answer{createAnswer("Lira", false), createAnswer("Pound", false), createAnswer("Euro", true)}
+	var answersGroupC []answer = []answer{createAnswer("Mediterranean ", true), createAnswer("Dead Sea", false), createAnswer("Sea of Samsara", false)}
+	var answersGroupD []answer = []answer{createAnswer("3", true), createAnswer("1", false), createAnswer("4", false)}
+	var answersGroupE []answer = []answer{createAnswer("Blue,White and Red", false), createAnswer("White and Red", true), createAnswer("Red and White", false)}
+
+	questions = append(questions, createQuestion("What ls the national language of Malta?", answersGroupA))
+	questions = append(questions, createQuestion("What currency is used in Malta?", answersGroupB))
+	questions = append(questions, createQuestion("In which sea is Malta located?", answersGroupC))
+	questions = append(questions, createQuestion("How many inhabited islands make up the Republic of Malta?", answersGroupD))
+	questions = append(questions, createQuestion("What are the colours of Malta's National flag?", answersGroupE))
+
+}
+
+var players []player
+var questions []question
+
+func main() {
+
+	initData()
 	router := gin.Default()
 	router.GET("/players", getPlayers)
 	router.POST("/players", setPlayer)
@@ -71,8 +116,9 @@ func setPlayer(c *gin.Context) {
 func getPlayer(c *gin.Context) {
 	var returnedPlayer player
 	inputName := c.Param("name")
+	playertype := reflect.TypeOf(inputName)
 
-	if reflect.TypeOf(inputName) == reflect.TypeOf(returnedPlayer.Name) {
+	if reflect.TypeOf(playertype) == reflect.TypeOf(returnedPlayer.Name) {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "The value you provided is not valid"})
 		return
 	}
@@ -80,6 +126,7 @@ func getPlayer(c *gin.Context) {
 	for i := 0; i < len(players); i++ {
 		if players[i].Name == inputName {
 			returnedPlayer = players[i]
+			break
 		}
 	}
 
